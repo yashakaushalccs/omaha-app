a01b1af224ad671d1e8fa83acf8a794006a7bfd3
Yasha Kaushal <ykaushal@ccsfundraising.com>
2025-05-15 23:19:07 -0400

    updated requirements

--- omaha_app.py
+++ omaha_app.py
@@ -12,7 +12,7 @@ from langchain.prompts import (
 )
 from langchain_core.output_parsers import StrOutputParser
 
-from langchain_community.vectorstores import Chroma
+# from langchain_community.vectorstores import Chroma
 from langchain_openai import OpenAIEmbeddings
 from langchain.schema.runnable import RunnablePassthrough
 from langchain_core.runnables import RunnableLambda
